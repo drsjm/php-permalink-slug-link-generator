4b5e52ab116474ffd8a8d06eec72df8f33f6280b
Daniel Rodrigues <87614122+drsjm@users.noreply.github.com>
2021-07-21 21:23:05 +0100

    Update example.php

--- example.php
+++ example.php
@@ -103,7 +103,7 @@ if ( isset($_POST['name']) ) {
 		</form>
 
 		<p>
-			<strong>If you want to thank me you can buying me a coffee at:</strong>
+			<strong>If you want to thank me...</strong>
 			<br>
 			<a href="https://www.buymeacoffee.com/drsjm" target="_blank">
 				<img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" >
